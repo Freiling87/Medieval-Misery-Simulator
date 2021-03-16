a3be240dd130e519ccbe0f668372d3a2d813096d
FN LN <lucas87t@gmail.com>
2021-03-16 16:45:29 -0500

    Basic work on Wiki node system

---
+++ Interface/Wiki/WikiCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MMS.Interface.Wiki
+{
+	class WikiCategory
+	{
+		WikiCategory parent;
+		WikiPage page;
+
+		public WikiCategory(string name, WikiCategory parent)
+		{
+			// Automatically create page when instantiated
+			page = (WikiPage)Activator.CreateInstance(null, name, name.ToUpper());
+
+		}
+	}
+}
